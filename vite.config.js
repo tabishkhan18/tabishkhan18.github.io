7c936c6612ff0aa63b16623d5d50cdb1c8efdfed
Tabishkhan18 <tabishkhan1811@gmail.com>
2025-02-22 19:13:08 +0530

    change repository

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/tabishkhan18.github.io/",
+  base: "/Tabishkhan18.github.io/",
 })
